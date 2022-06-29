27c240a157dc0632d0771ab65162c5a45c5d7af2
Farid Shamsov <farid979797@gmail.com>
2022-06-29 18:10:01 +0300

    add querries for categories and subcats

--- coolsite/shop/views.py
+++ coolsite/shop/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import logout, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -23,7 +24,6 @@ class ShopMain(DataMixin, ListView):
         return context
 
     def get_queryset(self):
-        print(Product.objects.filter(is_published=True).select_related('cat'))
         return Product.objects.filter(is_published=True)
 
 class ShowProduct(DataMixin, DetailView):
@@ -42,17 +42,26 @@ class ProductCategory(DataMixin, ListView):
     model = Product
     template_name = 'shop/index.html'
     context_object_name = 'products'
+    parent_id = 0
     #allow_empty = False
 
-    def get_queryset(self):
-        print(Product.objects.filter(cat__slug=self.kwargs['cat_slug']))
-        return Product.objects.filter(cat__slug=self.kwargs['cat_slug'])
-
     def get_context_data(self, *, object_list=None, **kwargs):  #для передачи динамических данных
+        print('get_context')
         context = super().get_context_data(**kwargs)
         c = Category.objects.get(slug=self.kwargs['cat_slug'])
         c_def = self.get_user_context(title='Категория - ' + str(c.name),
                                       cat_selected=c.pk)        #in utils.py  DataMixin
         context.update(c_def)
         return context
+
+    def get_queryset(self, object_list=None, **kwargs):
+        print('get_query')
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        if c.parent_category is None:
+            print('parent')
+            return Product.objects.filter(Q(cat__parent_category=c.pk) | (Q(pk=c.pk)))
+        else:
+            print('not parent')
+            return Product.objects.filter(cat__slug=self.kwargs['cat_slug'])
+
 
